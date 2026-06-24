61fa1557ec2e3535f03ddbe64d5c0a6faf045d43
过包顺顺顺 <15959281692@163.com>
2026-06-25 00:02:39 +0800

    Add project files part 13

---
+++ iooker/KEManager/KETooking/CGKeTookingManager.h
@@ -0,0 +1,18 @@
+//
+//  CGKeTookingManager.h
+//  iooker
+//
+//  Created by JJK on 2024/6/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGKeTookingManager : NSObject
+
++ (void)shareTool:(NSString *)Register;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iooker/KEManager/KETooking/CGKeTookingManager.m
@@ -0,0 +1,16 @@
+//
+//  CGKeTookingManager.m
+//  iooker
+//
+//  Created by JJK on 2024/6/12.
+//
+
+#import "CGKeTookingManager.h"
+
+@implementation CGKeTookingManager
+
++ (void)shareTool:(NSString *)Register {
+    
+}
+
+@end
